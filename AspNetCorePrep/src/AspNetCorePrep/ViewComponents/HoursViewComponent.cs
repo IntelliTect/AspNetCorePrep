a6101e487c40c88a8c8315bd1d172d779b1f32ec
Grant Erickson <grant@intellitect.com>
2016-04-26 15:01:05 -0700

    Added ViewComponent

---
+++ AspNetCorePrep/src/AspNetCorePrep/ViewComponents/HoursViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCorePrep.Models;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AspNetCorePrep.ViewComponents
+{
+    public class HoursViewComponent: ViewComponent
+    {
+        private ApplicationDbContext _context;
+
+        public HoursViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            
+            return View(_context.TimeEntries.Where(f=>f.ApplicationUserId == HttpContext.User.GetUserId()).Sum(f=>f.Hours));
+        }
+    }
+}
