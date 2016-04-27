177a61f7fd49b446e2fa4f0b8af2972e20345220
Grant Erickson <grant@intellitect.com>
2016-04-27 12:41:01 -0700

    Unit test project added with simple unit test.

---
+++ AspNetCorePrep/AspNetCorePrep.Tests/CustomerTest.cs
@@ -0,0 +1,26 @@
+﻿using AspNetCorePrep.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCorePrep.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class CustomerTest
+    {
+        public CustomerTest()
+        {
+        }
+
+        [Fact]
+        public void CustomerNameTest()
+        {
+            var c = new Customer() { Name = "Test" };
+            Assert.True(c.Name == "Test");
+        }
+
+    }
+}
