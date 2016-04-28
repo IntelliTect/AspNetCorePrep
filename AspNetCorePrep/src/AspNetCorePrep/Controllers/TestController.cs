b91505117b591625900767954de4d10221323598
Grant Erickson <grant@intellitect.com>
2016-04-28 10:10:09 -0700

    Static MVC view with API.

---
+++ AspNetCorePrep/src/AspNetCorePrep/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCorePrep.Controllers
+{
+    public class TestController: Controller
+    {
+        public IActionResult Index()
+        {
+            int count = 10;
+            return View(count);
+        }
+
+        public string Concat(int a, string b = " test")
+        {
+            return $"{a} is {b}";
+        }
+
+    }
+}
